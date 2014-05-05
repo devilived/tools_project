f27adbd468380bf3cb66bab0dff0b528899ba305
xingqisheng <xingqisheng@vidmt.com>
2014-05-05 11:06:01 +0800

    update the apktools and dex2jar

--- decodeapk/dapk.bat
+++ decodeapk/dapk.bat
@@ -2,17 +2,17 @@ set tgt_dir=%~n1
 
 rd /s/q %tgt_dir%
 
-java -jar "apktool\apktool.jar" d %1
+java -jar "apktool1.5.2\apktool.jar" d %1
 
 rd /s/q %tgt_dir%\smali
 del %tgt_dir%\apktool.yml
 
 
-"C:\Program Files\7-Zip\7z" e %1 classes.dex -y
-call dex2jar-0.0.9.8\dex2jar classes.dex
+"C:\Program Files\WinRAR\WinRAR.exe" e %1 classes.dex -y
+call dex2jar-0.0.9.15\dex2jar classes.dex
 
-copy classes_dex2jar.jar %tgt_dir%
+copy classes_dex2jar.jar "%tgt_dir%"
 
 del classes.dex classes_dex2jar.jar
 
-jd-gui %tgt_dir%\classes_dex2jar.jar
+jd-gui "%tgt_dir%\classes_dex2jar.jar"
