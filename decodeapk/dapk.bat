b9a3f234376f6aa8aa3cef1d2c46a9fbefc6acc5
xqs <xingqisheng@vidmt.com>
2019-03-28 18:45:45 +0800

    tools->toolls

--- decodeapk/dapk.bat
+++ decodeapk/dapk.bat
@@ -5,17 +5,17 @@ rd /s/q %tgt_dir%
 
 set apktool_ver=2.3.3
 
-java -jar  -Duser.language=en "apktool_%apktool_ver%\apktool_%apktool_ver%.jar" d %1
+java -jar  -Duser.language=en "tools\apktool_%apktool_ver%\apktool_%apktool_ver%.jar" d %1
 
 ::rd /s/q %tgt_dir%\smali
 ::del %tgt_dir%\apktool.yml
 
 md %tgt_dir%\out
 "D:\Program Files\7-Zip\7z.exe" e %1 -o%tgt_dir%\out classes.dex -y
-call dex2jar-2.0\d2j-dex2jar.bat -o %tgt_dir%\out\classes-dex2jar.jar %tgt_dir%\out\classes.dex
+call tools\dex2jar-2.0\d2j-dex2jar.bat -o %tgt_dir%\out\classes-dex2jar.jar %tgt_dir%\out\classes.dex
 del classes-error.zip %tgt_dir%\out\classes.dex
 
-jd-gui.exe "%tgt_dir%\out\classes-dex2jar.jar"
+tools\jd-gui.exe "%tgt_dir%\out\classes-dex2jar.jar"
 
-del jd-gui.cfg
+del tools\jd-gui.cfg
 
